2de1d5d0f40de6c357cdc401a3b0f77aa0e2226d
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2022-05-19 20:38:48 +0300

    Update roadRadar.js

--- SyntaxFunctionsStatementsEx/roadRadar.js
+++ SyntaxFunctionsStatementsEx/roadRadar.js
@@ -1,37 +1,29 @@
 function speed(speed, area) {
     let speedLimit = 0;
-    let isSpeeding = false;
+    let isSpeeding = true;
     switch (area) {
         case "motorway":
             speedLimit = 130
             if (speed <= speedLimit) {
                 isSpeeding = false;
-            } else {
-                isSpeeding = true;
             }
             break;
         case "interstate":
             speedLimit = 90;
             if (speed <= speedLimit) {
                 isSpeeding = false;
-            } else {
-                isSpeeding = true;
             }
             break;
         case "city":
             speedLimit = 50;
             if (speed <= speedLimit) {
                 isSpeeding = false;
-            } else {
-                isSpeeding = true;
             }
             break;
         case "residential":
             speedLimit = 20;
             if (speed <= speedLimit) {
                 isSpeeding = false;
-            } else {
-                isSpeeding = true;
             }
             break;
     }
